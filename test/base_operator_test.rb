2413870c486e5950b10908f4a83738a5311e4b63
Florian Koch <fkoch@selectline.de>
2017-07-24 12:36:35 +0200

    added missing tests, updated gems

--- test/base_operator_test.rb
+++ test/base_operator_test.rb
@@ -11,4 +11,32 @@ class BaseOperatorTest < Minitest::Test
     end
     assert_kind_of CriteriaOperator::NotImplementedError, err
   end
+
+  def test_serialize
+    serialized_op = @operator.serialize
+    assert !serialized_op.nil?, 'Serialization returned nil!'
+    assert_kind_of String, serialized_op
+    begin
+      op = YAML.safe_load serialized_op, (ObjectSpace.each_object(Class).select { |klass| klass <= CriteriaOperator::BaseOperator })
+    rescue
+      assert false, 'Serialized operator couldn\'t be deserialized!'
+    end
+    assert_kind_of CriteriaOperator::BaseOperator, op
+  end
+
+  def test_deserialize
+    serialized_op = YAML.dump(CriteriaOperator::OperandValue.new(42))
+    op = CriteriaOperator::BaseOperator.deserialize(serialized_op)
+    assert !op.nil?, 'Deserialization returned nil!'
+    assert_kind_of CriteriaOperator::OperandValue, op
+    assert_equal 42, op.value
+  end
+
+  def test_deserialize_fail
+    serialized_obj = DateTime.new.to_yaml
+    err = assert_raises Psych::Exception do
+      CriteriaOperator::BaseOperator.deserialize(serialized_obj)
+    end
+    assert_kind_of Psych::DisallowedClass, err
+  end
 end
